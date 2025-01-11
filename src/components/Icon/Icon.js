f0f7a9595ffee5aff63122181874ea4a1c76de1b
voyl_x <channattmad@gmail.com>
2025-01-12 01:45:29 +0700

    [fix] className, fillRule

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -187,17 +187,17 @@ export const People = (props) => {
 
     >
         <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M16.67 13.13C18.04 14.06 19 15.32 19 17v3h3c.55 0 1-.45 1-1v-2c0-2.18-3.57-3.47-6.33-3.87"
         />
         <circle
             cx="9"
             cy="8"
             r="4"
-            fill-rule="evenodd"
+            fillRule="evenodd"
         />
         <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4c-.47 0-.91.1-1.33.24a5.98 5.98 0 0 1 0 7.52c.42.14.86.24 1.33.24m-6 1c-2.67 0-8 1.34-8 4v2c0 .55.45 1 1 1h14c.55 0 1-.45 1-1v-2c0-2.66-5.33-4-8-4"
         />
     </svg>
@@ -228,7 +228,7 @@ export const ThaiBaht = (props) => {
 export const Facebook = (props) => {
 
     return <svg
-        classname="w-4 h-4"
+        className="w-4 h-4"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
@@ -249,7 +249,7 @@ export const Facebook = (props) => {
 export const Discord = (props) => {
 
     return <svg
-        classname="w-4 h-4"
+        className="w-4 h-4"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
@@ -266,7 +266,7 @@ export const Discord = (props) => {
 export const Store = (props) => {
 
     return <svg
-        classname="w-6 h-6 text-gray-800 dark:text-white"
+        className="w-6 h-6 text-gray-800 dark:text-white"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         width={24}
