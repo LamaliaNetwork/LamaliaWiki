47d39ebc4efc8ddc7f1ccc215c5506c03a9a0ea8
voyl_x <channattmad@gmail.com>
2025-01-14 14:01:58 +0700

    [feat] map เพิ่มปุ่ม (ยังไม่เสร็จ)

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -2,23 +2,111 @@ import clsx from "clsx"
 import Layout from "@theme/Layout"
 import styles from "./index.module.css"
 import Iframe from "../components/Iframe"
+import { Button, Col, Collapse, ConfigProvider, Flex, Input, InputNumber, Row, Select } from "antd"
+import { useState } from "react"
+
+const worldList = [
+	{
+		value: 'minecraft_overworld',
+		label: 'BuildWorld',
+		min: -35000,
+		max: 35000
+	},
+	{
+		value: 'minecraft_the_nether',
+		label: 'Nether',
+		min: -10000,
+		max: 10000
+	},
+	{
+		value: 'minecraft_the_end',
+		label: 'TheEnd',
+		min: -10000,
+		max: 10000
+	},
+]
 
 export default function MapPage() {
-  return (
-    <Layout>
-      <main>
-        <header className={clsx("hero hero--black", styles.heroBanner)}>
-          <div className="container">
-            <span className="content">
-              <h2>Map</h2>
-              <Iframe
-                src="https://map.lamalia.net/"
-                title="Lamalia Map"
-              ></Iframe>
-            </span>
-          </div>
-        </header>
-      </main>
-    </Layout>
-  )
+
+	const path = 'https://map.lamalia.net/?world=minecraft_overworld&zoom=2&x=-2848&z=21762'
+
+	const initState = {
+		world: worldList[0].value,
+		xAxis: 0,
+		xAxis: 0
+	}
+	const [state, setState] = useState(initState)
+
+	const items = [
+		{
+			key: '1',
+			label: 'ใส่คำว่าอะไรดีคิดไม่ออก',
+			children: <MapCustomValue state={state} setState={setState} />,
+		}
+	]
+
+	return (
+		<ConfigProvider
+			theme={{
+				components: {
+					Collapse: {
+						headerBg: '#ffffff'
+					},
+				},
+			}}>
+
+			<Layout>
+				<main>
+					<header className={clsx("hero hero--black", styles.heroBanner)}>
+						<div className="container">
+							<span className="content">
+								<h2>Map</h2>
+								<Flex justify="center">
+									<Col md={18} xs={20}>
+										<Collapse items={items}
+											defaultActiveKey={1}	//เดี๋ยวมาเอาออก
+										/>
+									</Col>
+								</Flex>
+
+								<Iframe
+									src={path}
+									title="Lamalia Map"
+									style={{ marginTop: '1em' }}
+								></Iframe>
+							</span>
+						</div>
+					</header>
+				</main>
+			</Layout>
+		</ConfigProvider>
+	)
+}
+
+function MapCustomValue() {
+	return (
+		<Row justify="center" gutter={{
+			xs: 8,
+			sm: 16,
+			md: 24,
+			lg: 32,
+		}}>
+			<Col span={6} >
+				<InputNumber placeholder="x-Axist"
+					style={{ width: '100%' }}
+					max={1000} />
+			</Col>
+			<Col span={6}>
+				<InputNumber placeholder="y-Axist"
+					style={{ width: '100%' }}
+					max={1000} />
+			</Col>
+			<Col span={6}>
+				<Select
+					options={worldList.map(({ value, lable }) => ({ value, lable }))}
+					style={{ width: '100%' }}
+				/>
+			</Col>
+		</Row>
+	)
 }
