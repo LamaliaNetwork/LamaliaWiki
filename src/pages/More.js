e0d936347b050b584d5e26bd46b859fd22ddef75
voyl_x <channattmad@gmail.com>
2025-01-14 14:32:15 +0700

    [fix] MorePage getParames in useEffect

--- src/pages/More.js
+++ src/pages/More.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react"
 
 export default function EmbedPage() {
 
-    const queryParameters = new URLSearchParams(window.location.search)
-    const page = queryParameters.get("page")
-
     const [renderer, setRenderer] = useState(null)
 
     useEffect(() => {
+
+        const queryParameters = new URLSearchParams(window.location.search)
+        const page = queryParameters.get("page")
+
         if (page === 'Roadmap') setRenderer(<>Roadmap Page</>)
         else if (page === 'Map') setRenderer(<>Map Page</>)
         else window.location.href = '/Notfound'
