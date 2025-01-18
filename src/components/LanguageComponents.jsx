399e63edc85fdc862bee4e2e1e0f91ba0c64a613
voyl_x <channattmad@gmail.com>
2025-01-18 20:26:10 +0700

    [fix] dispenser เขียน 2ภาษาในไฟลเดียว

---
+++ src/components/LanguageComponents.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+// ฟังก์ชันแสดงเนื้อหาภาษาไทย
+export const TH = ({ children }) => {
+  const { i18n } = useDocusaurusContext();
+  const { currentLocale } = i18n;
+  return currentLocale === 'th' ? <div>{children}</div> : null;
+};
+
+// ฟังก์ชันแสดงเนื้อหาภาษาอังกฤษ
+export const EN = ({ children }) => {
+  const { i18n } = useDocusaurusContext();
+  const { currentLocale } = i18n;
+  return currentLocale === 'en' ? <div>{children}</div> : null;
+};
