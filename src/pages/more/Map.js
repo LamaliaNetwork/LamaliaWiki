9bac88132ca3dcddc3e5645e24f74d86d83bacfc
voyl_x <channattmad@gmail.com>
2025-01-19 01:37:46 +0700

    [fix] map Coordinate selecter

--- src/pages/more/Map.js
+++ src/pages/more/Map.js
@@ -3,7 +3,7 @@ import Layout from "@theme/Layout"
 import styles from "./../index.module.css"
 import Iframe from "../../components/Iframe"
 import { Button, Col, Collapse, ConfigProvider, Flex, Input, InputNumber, Row, Select } from "antd"
-import { useState } from "react"
+import { useMemo, useState } from "react"
 
 const worldList = [
 	{
@@ -28,20 +28,28 @@ const worldList = [
 
 export default function MapPage() {
 
-	const path = 'https://map.lamalia.net/'
-
 	const initState = {
+		basePath: 'https://map.lamalia.net/',
+		path: 'https://map.lamalia.net/',
 		world: worldList[0].value,
+		showIframe: true,
 		xAxis: 0,
-		yAxis: 0
+		zAxis: 0
 	}
 	const [state, setState] = useState(initState)
+
+	const handleChangeState = (value, key) => {
+		setState((prev) => ({
+			...prev,
+			[key]: value
+		}))
+	}
 
 	const items = [
 		{
 			key: '1',
 			label: 'Coordinate X, Z',
-			children: <MapCustomValue state={state} setState={setState} />,
+			children: <MapCustomValue state={state} handleChangeState={handleChangeState} />,
 		}
 	]
 
@@ -61,19 +69,21 @@ export default function MapPage() {
 						<div className="container">
 							<span className="content">
 								<h2>Map</h2>
-								{/* <Flex justify="center">
+								<Flex justify="center">
 									<Col md={18} xs={20}>
 										<Collapse items={items}
 											defaultActiveKey={1}	//เดี๋ยวมาเอาออก
 										/>
 									</Col>
-								</Flex> */}
-
-								<Iframe
-									src={path}
-									title="Lamalia Map"
-									style={{ marginTop: '1em' }}
-								></Iframe>
+								</Flex>
+								{
+									state.showIframe &&
+									<Iframe
+										src={state.path}
+										title="Lamalia Map"
+										style={{ marginTop: '1em' }}
+									></Iframe>
+								}
 							</span>
 						</div>
 					</header>
@@ -83,29 +93,55 @@ export default function MapPage() {
 	)
 }
 
-function MapCustomValue() {
+function MapCustomValue({ state, handleChangeState }) {
+	const worldData = useMemo(() => {
+		return worldList.find((el) => el.value === state.world)
+	}, [state.world])
+
+	const handleClickGo = () => {
+		const newPath = `${state.basePath}/?world=${state.world}&zoom=3&x=${state.xAxis}&z=${state.zAxis}`
+		handleChangeState(newPath, 'path')
+	}
+
 	return (
-		<Row justify="center" gutter={{
-			xs: 8,
-			sm: 16,
-			md: 24,
-			lg: 32,
-		}}>
-			<Col span={6} >
-				<InputNumber placeholder="x-Axist"
+		<Row
+			justify="center"
+			gutter={{
+				xs: 8,
+				sm: 16,
+				md: 24,
+				lg: 32,
+			}}
+			style={{ marginTop: '1rem'}}
+		>
+			<Col span={6}>
+				<InputNumber
+					placeholder="x-Axis"
 					style={{ width: '100%' }}
-					max={1000} />
+					max={worldData?.max || 1000}
+					min={worldData?.min || -1000}
+					onChange={(e) => handleChangeState(e, 'xAxis')}
+				/>
 			</Col>
 			<Col span={6}>
-				<InputNumber placeholder="y-Axist"
+				<InputNumber
+					placeholder="z-Axis"
 					style={{ width: '100%' }}
-					max={1000} />
+					max={worldData?.max || 1000}
+					min={worldData?.min || -1000}
+					onChange={(e) => handleChangeState(e, 'zAxis')}
+				/>
 			</Col>
 			<Col span={6}>
 				<Select
-					options={worldList.map(({ value, lable }) => ({ value, lable }))}
+					options={worldList.map(({ value, label }) => ({ value, label }))}
 					style={{ width: '100%' }}
+					defaultValue={state.world}
+					onChange={(value) => handleChangeState(value, 'world')}
 				/>
+			</Col>
+			<Col>
+				<Button onClick={handleClickGo}>Go!</Button>
 			</Col>
 		</Row>
 	)
