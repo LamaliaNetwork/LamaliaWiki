fcb834d7ab371636f380915cf42428da5e47bb4d
Kitt-n <kittichot.socialcontact@gmail.com>
2025-01-28 00:46:06 +0700

    Add SEO
    -WE_Tool
    -High Speed Rail

---
+++ src/components/SEO/info_seo.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
+
+const WE_Tool = () => {
+    return (
+        <Head>
+            <meta name="description" content="It is a tool that facilitates faster building construction." />
+            <meta name="keywords" content="Tool, Build, Builder, Upgrades, World edit upgrades, World edit tool " />
+            <meta name="author" content="Lamalia Team." />
+        </Head>
+    );
+};
+
+export { WE_Tool };
+
+const HighSpeedRail = () => {
+    return (
+        <Head>
+            <meta name="description" content="Speed improvements have been implemented in the tram transport system." />
+            <meta name="keywords" content="High, High speed, Rail, minecart, gold, Gold block, speed, Obsidian block, Super powered rail, Super stopper rail " />
+            <meta name="author" content="Lamalia Team." />
+        </Head>
+    );
+};
+
+export { HighSpeedRail };
